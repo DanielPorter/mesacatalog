5f969fd489546a01cc4599679bfd3ac3edafdd11
Daniel Porter <82287748+DanielPorter@users.noreply.github.com>
2024-04-03 10:52:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
   const courseCards = document.querySelectorAll('.course');
 
   courseCards.forEach(courseCard => {
-    const toggleButton = courseCard.querySelector('.toggle-description');
     const description = courseCard.querySelector('.description');
-    const courseDescription = courseCard.querySelector('.course-description');
-    const courseUnits = courseCard.querySelector('.course-units');
-
-    toggleButton.addEventListener('click', () => {
-      description.classList.toggle('hidden');
-      if (description.classList.contains('hidden')) {
-        toggleButton.textContent = 'Show Details';
-      } else {
-        toggleButton.textContent = 'Hide Details';
-      }
-    });
 
     courseCard.addEventListener('click', () => {
-      courseDescription.textContent = courseCard.getAttribute('data-description');
-      courseUnits.textContent = courseCard.getAttribute('data-units');
+      description.classList.toggle('hidden');
     });
   });
 });
